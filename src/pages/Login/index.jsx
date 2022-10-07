1fadcba85a6421f48e600b469863e4d2a9391867
CesarCostaPires <88549910+CesarCostaPires@users.noreply.github.com>
2022-10-07 17:35:09 -0300

    Update index.jsx

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,17 +7,45 @@ const image = { uri: "https://img.freepik.com/fotos-premium/a-textura-da-tela-pr
 
 //const image = { uri: "https://img.freepik.com/vetores-gratis/copie-o-fundo-digital-dos-circuitos-azuis-do-espaco_23-2148821699.jpg?w=740&t=st=1661290825~exp=1661291425~hmac=9be0803ee93e7a7f5088d25a319edcccaee85e2fc534c09c856b363c7228f643" };
 export default function Login(){
-    let[senha, setSenha] = useState('');
+    let[senhaEmpresa, setSenha] = useState('');
+    let[emailEmpresa, setEmail] = useState('');
     const navigation = useNavigation();
+    async function Logar(){ 
+  
 
-    function Logar(){
-        if(senha == "Moto"){
-            navigation.navigate('Motorista');
-        }
-        else if(senha == "Empresa"){
-            navigation.navigate('Empresa');
-        }
-    }
+        console.log("clicou"); 
+        
+        let response = await api.post('login.php', { 
+        
+        email: emailEmpresa, 
+        senha: senhaEmpresa 
+        }) 
+        
+        .then(function (response) { 
+          
+        
+        //Verificando o valor da variavel mensagem enviada pelo php 
+        if(response.data == 'Logado com Sucesso!'){ 
+        Alert.alert("Login realizado com sucesso!"); 
+        navigation.navigate('Empresa'); 
+        } 
+         else if(response.data == 'Erro ao logar') 
+         Alert.alert("E-mail ou senha incorreto, tente novamente"); 
+        console.log(response); 
+        }); }
+    //function Logar(){ 
+
+        //Alert.alert("Login efetuado com sucesso!"); 
+        //navigation.navigate('Empresa') 
+     //} 
+    //function Logar(){
+        //if(senha == " "){
+            //navigation.navigate('Motorista');
+        //}
+      // else if(senha == " "){
+            //navigation.navigate('Empresa');
+       // }
+    //}
     function goEmpresaMotorista(){
         navigation.navigate('EmpresaMotorista');
     }
@@ -27,7 +55,7 @@ export default function Login(){
         <ContainerLogin>
              <ImageBackground source={image} resizeMode="cover" style={styles.image}>
              <ViewCaixas>
-                <CaixaTexto placeholder="Digite seu email"  CaixaTexto/>
+                <CaixaTexto placeholder="Digite seu email"   onChangeText={setEmail}/>
                 <CaixaTexto placeholder="Digite sua senha" onChangeText={setSenha} />
             </ViewCaixas>
             <ViewBotoes>
