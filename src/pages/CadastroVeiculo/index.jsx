73f1e12d7a53c164d04a875a28ab6543ee518073
CesarCostaPires <88549910+CesarCostaPires@users.noreply.github.com>
2022-10-14 17:08:44 -0300

    Update index.jsx

--- src/pages/CadastroVeiculo/index.jsx
+++ src/pages/CadastroVeiculo/index.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useNavigation} from '@react-navigation/native'; 
-import {ImageBackground,TextInput, Text, TouchableOpacity, View, StyleSheet, Image} from 'react-native';
+import {ImageBackground,TextInput, Text, TouchableOpacity, View, Alert, StyleSheet, Image} from 'react-native';
 import { CampoMedio, Botao, TextoBotao, CampoGrande, CampoPequeno, Scrollzinho, ContainerCadastro, TituloMotorista, TituloEntregas, TextoDadosMotorista, ViewCadastro, ViewCep, BotaoCep } from "../Style";
+import api from "../../services/api";
 //const image = { uri: "https://img.freepik.com/vetores-gratis/copie-o-fundo-digital-dos-circuitos-azuis-do-espaco_23-2148821699.jpg?w=740&t=st=1661290825~exp=1661291425~hmac=9be0803ee93e7a7f5088d25a319edcccaee85e2fc534c09c856b363c7228f643" };
 const image = { uri: "https://img.freepik.com/fotos-premium/a-textura-da-tela-preta-escura-para-a-imagem-do-design_99266-547.jpg?w=740" };
 
@@ -11,11 +12,26 @@ export default function CadastroVeiculo(){
     let [modeloVeiculo, setModelo] = useState();
     let [capaVeiculo, setCapacidade] = useState();
     let [chassiVeiculo, setChassi] = useState();
-    const navigation = useNavigation();
+    async function CadastrarUsuario(){
+        console.log("clicou");
 
-    function goLogin(){
-        navigation.navigate('Login');
+        let response = await api.post('cadastroVeiculo.php', {
+            placaVeiculo: placaVeiculo,
+            anoVeiculo: anoVeiculo,
+            modeloVeiculo: modeloVeiculo, 
+            capaVeiculo: capaVeiculo,  
+            chassiVeiculo:chassiVeiculo
+        }).then(function(response){Alert.alert("Usuario cadastrado com sucesso!");
+    console.log(response);
+        }).catch(function(error){
+            Alert.alert("Erro ao cadastrar,tente mais tarde!");
+            console.log(error);
+        });
+
+        
+        navigation.navigate('Inicial');
     }
+    const navigation = useNavigation()
     return(
         <ContainerCadastro>
             <ImageBackground source={image} resizeMode="cover" style={styles.image}>
@@ -32,7 +48,7 @@ export default function CadastroVeiculo(){
                     <CampoMedio onChangeText={setCapacidade}/>
                     <TextoDadosMotorista>Chassi:</TextoDadosMotorista>
                     <CampoGrande onChangeText={setChassi}/>
-                    <Botao onPress={goLogin}><TextoBotao>Cadastrar veículo</TextoBotao></Botao>
+                    <Botao onPress={CadastrarUsuario}><TextoBotao>Cadastrar veículo</TextoBotao></Botao>
                 </Scrollzinho>
             </ViewCadastro>
             </ImageBackground>
